4191f8158c5daf7f7b91905efe51e669f7b78f5e
Vinicius Faria <viniciusfariaresende@gmail.com>
2023-11-16 22:57:34 -0300

    feat: implementing features to pass test8

--- src/pager.c
+++ src/pager.c
@@ -22,12 +22,15 @@
 struct page_table_cell {
   short valid;
   short present;
+  short prot;
+  short recently_accessed;
   __intptr_t page;
   int frame;
 };
 
 struct process_data {
   pid_t pid;
+  size_t frames_allocated;
   struct page_table_cell *page_table;
 };
 
@@ -47,11 +50,13 @@ struct Node* createNode(pid_t pid) {
   for (int i = 0; i< NUM_PAGES; i++) {
     page_table[i].valid = 0;
     page_table[i].present = 0;
+    page_table[i].recently_accessed = 0;
     page_table[i].page = -1;
     page_table[i].frame = -1;
   }
   newNode->data.page_table = page_table;
   newNode->data.pid = pid;
+  newNode->data.frames_allocated = 0;
 	newNode->next = NULL;
 
 	return newNode;
@@ -119,6 +124,24 @@ struct page_table_cell* searchByPage(struct Node* head, pid_t pid, __intptr_t pa
   return NULL;
 }
 
+int searchLeastFrequentlyUsedFrameIdx(struct Node* process, int initial) { 
+  if(process == NULL) return 0;
+
+  size_t i = initial;
+  while (1) {
+    i = (i + 1) % process->data.frames_allocated;
+
+    if(process->data.page_table[i].present != 0) {
+      if (process->data.page_table[i].recently_accessed == 0) {
+        return i;
+      }
+
+      process->data.page_table[i].recently_accessed = 0;
+    }  
+
+  }
+}
+
 void printList(struct Node* head) {
 	struct Node* current = head;
 
@@ -143,8 +166,8 @@ int free_frames;
 int *blocks_vector;
 int blocks_vector_size;
 int free_blocks;
+int last_freed_frame_idx = -1;
 struct Node* head_process = NULL;
-
 
 void pager_init(int nframes, int nblocks) {
   if (nframes <= 0 || nblocks <= 0) {
@@ -182,7 +205,6 @@ void *pager_extend(pid_t pid) {
       if (frames_vector[i] == -1) {
         frames_vector[i] = pid;
         free_idx = i;
-        free_frames--;
         break;
       }
     }
@@ -190,7 +212,6 @@ void *pager_extend(pid_t pid) {
 
   struct Node *process_node = searchByPid(head_process, pid);
   if (process_node == NULL) {
-    printf("Process not found\n");
     exit(EXIT_FAILURE);
   }
 
@@ -230,6 +251,35 @@ void pager_destroy(pid_t pid) {
 	removeProcess(head_process, pid);
 }
 
+void _handleSwap(struct Node *process_node, int cell_idx) {
+  struct page_table_cell *page_cell = &process_node->data.page_table[cell_idx];
+  
+  last_freed_frame_idx = searchLeastFrequentlyUsedFrameIdx(process_node, last_freed_frame_idx);
+  struct page_table_cell *last_freed_cell = &process_node->data.page_table[last_freed_frame_idx];
+          
+  if(last_freed_cell->prot != PROT_NONE) {
+    size_t it = last_freed_frame_idx;
+    
+    while(process_node->data.page_table[it].prot != PROT_NONE) {
+      if(process_node->data.page_table[it].present) {
+        mmu_chprot(process_node->data.pid, (void *) process_node->data.page_table[it].page, PROT_NONE);
+        process_node->data.page_table[it].prot = PROT_NONE;
+      }
+
+      it = (it + 1) % process_node->data.frames_allocated;
+    }
+  }
+
+  mmu_nonresident(process_node->data.pid, (void *) last_freed_cell->page);
+  mmu_zero_fill(last_freed_cell->frame);
+  last_freed_cell->present = 0;
+
+  page_cell->frame = last_freed_cell->frame;
+  
+  mmu_resident(process_node->data.pid, (void *) page_cell->page, page_cell->frame, PROT_READ);
+  page_cell->prot = PROT_READ;
+}
+
 void pager_fault(pid_t pid, void *addr) {
   struct Node *process_node = searchByPid(head_process, pid);
 
@@ -237,14 +287,25 @@ void pager_fault(pid_t pid, void *addr) {
     struct page_table_cell *page_cell = &process_node->data.page_table[i];
     if (page_cell->page == (intptr_t) addr) {
       if(page_cell->valid == 0) {
-        mmu_zero_fill(page_cell->frame);
-        mmu_resident(pid, (void *) page_cell->page, page_cell->frame, PROT_READ);
+        if(free_frames > 0) {
+          mmu_zero_fill(page_cell->frame);
+          mmu_resident(pid, (void *) page_cell->page, page_cell->frame, PROT_READ);
+          page_cell->prot = PROT_READ;
+          free_frames--;
+        } else {
+          _handleSwap(process_node, i);
+        }
+        
         page_cell->valid = 1;
         page_cell->present = 1;
+        process_node->data.frames_allocated++;
         return;
       } else if (page_cell->present == 1) {
         mmu_chprot(pid, (void *) page_cell->page, PROT_READ | PROT_WRITE);
+        page_cell->prot = PROT_READ | PROT_WRITE;
       } else if (page_cell->present == 0) {
+        _handleSwap(process_node, i);
+        page_cell->present = 1;
         return;
       } 
     }
